fd35042aafd6f741154f68fa9f08f36e650dde78
willy <willington.londono@ucaldas.edu.co>
2022-04-07 20:30:00 -0500

    Primer commit

---
+++ src/test/java/edu/ucaldas/clientes/ClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ucaldas.clientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
